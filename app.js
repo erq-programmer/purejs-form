3d36bbe83ade9e81f39895260dcc3e4357039d5e
Eryk Słociński <eryk.slocinski@gmail.com>
2021-02-08 16:21:45 +0100

    Fixed login.svg colors, added main form logic, added styles for form inputs

--- app.js
+++ app.js
@@ -2,6 +2,36 @@ import { route } from './router';
 
 route('/', 'login', function () {
   this.title = 'PureJS Form';
+  this.$on('.login__form', 'submit', (e) => {
+    e.preventDefault();
+    const username = document.querySelector('#username');
+    const password = document.querySelector('#password');
+
+    const data = {
+      username: username.value,
+      password: password.value,
+    };
+
+    data &&
+      fetch('https://zwzt-zadanie.netlify.app/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+
+        .then((data) => {
+          data.error ? console.log('Error: ', data) : console.log('Success: ', data);
+        })
+        .catch((err) => {
+          console.error('There has been a problem with your fetch operation:', err);
+        });
+
+    const loginForm = document.querySelector('.login__form');
+    loginForm.reset();
+  });
 });
 
 route('/success', 'success', function () {
