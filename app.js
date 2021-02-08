70a9e5e2deb2764b018668babac0081d4240d6ec
Eryk Slocinski <eryk.slocinski@gmail.com>
2021-02-08 08:10:12 +0100

    Added properly routes and display on login screen form with username and password

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 import { route } from './router';
 
-route('/', 'home', function() {
-  this.where = 'here';
+route('/', 'login', function () {
+  this.title = 'PureJS Form';
 });
 
-route('/ex1', 'example1', function() {
-  this.title = 'Example 1';
+route('/success', 'success', function () {
+  this.title = 'Success Info';
 });
 
-route('/ex2', 'example2', function() {
-  this.title = 'Example 2';
-  this.counter = 0;
-  this.$on('.my-button', 'click', () => {
-    this.counter += 1;
-    this.$refresh();
-  });
+route('*', '404', function () {
+  this.title = 'Page not found!';
 });
-
-route('*', '404', function () {});
