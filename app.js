468d834084f1321c1a169cac74508945ef1c8e6d
Eryk Słociński <eryk.slocinski@gmail.com>
2021-02-09 14:45:13 +0100

    Added custom form validation

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 import { route } from './router';
 import { notFound, login, success } from './assets/templates';
 
-let scrp = document.createElement('script');
-scrp.type = 'text/html';
-scrp.id = '404';
-scrp.innerHTML = notFound;
-document.head.appendChild(scrp);
-
 route('/', 'login', function () {
   let script = document.createElement('script');
   script.type = 'text/html';
@@ -15,17 +9,82 @@ route('/', 'login', function () {
   document.head.appendChild(script);
 
   this.title = 'PureJS Form';
+
+  let isSubmit = false;
+
+  const setSubmit = () => {
+    isSubmit = !isSubmit;
+  };
+
+  const togglePassword = () => {
+    const passwordInput = document.querySelector('#password');
+    const togglePasswordButton = document.querySelector('.toggle-password');
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      togglePasswordButton.textContent = 'Hide';
+      togglePasswordButton.setAttribute('aria-label', 'Hide password');
+    } else {
+      passwordInput.type = 'password';
+      togglePasswordButton.textContent = 'Show';
+      togglePasswordButton.setAttribute('aria-label', 'Show password as plain text');
+    }
+  };
+
+  const checkInput = (inputString) => {
+    const input = document.getElementById(inputString);
+    const inputValue = input.value.trim();
+
+    function setError(input, message) {
+      const labelControl = input.parentElement;
+      const span = labelControl.querySelector('.label__error');
+      input.className = 'form__input form__input--error';
+      span.className = 'label__error label__error--visible';
+      span.innerText = message;
+    }
+
+    function setSuccess(input) {
+      const labelControl = input.parentElement;
+      const span = labelControl.querySelector('.label__error');
+      input.className = 'form__input form__input--success';
+      span.className = 'label__error';
+      span.innerText = 'There are no errors!';
+    }
+
+    if (inputValue === '') {
+      setError(input, `${inputString} cannot be blank`);
+    } else {
+      setSuccess(input);
+    }
+  };
+
+  this.$on('.toggle-password', 'click', () => {
+    togglePassword();
+  });
+
+  this.$on('#password', 'change', () => {
+    checkInput('password');
+  });
+
+  this.$on('#username', 'change', () => {
+    checkInput('username');
+  });
+
   this.$on('.login__form', 'submit', (e) => {
     e.preventDefault();
-    const username = document.querySelector('#username');
-    const password = document.querySelector('#password');
+
+    const loginForm = document.querySelector('.login__form');
+    const usernameInput = document.querySelector('#username');
+    const passwordInput = document.querySelector('#password');
 
     const data = {
-      username: username.value,
-      password: password.value,
+      username: usernameInput.value.trim(),
+      password: passwordInput.value.trim(),
     };
 
-    data &&
+    if (data.username === '' || data.password === '') {
+      checkInput('password');
+      checkInput('username');
+    } else {
       fetch('https://zwzt-zadanie.netlify.app/api/login', {
         method: 'POST',
         headers: {
@@ -36,15 +95,13 @@ route('/', 'login', function () {
         .then((response) => response.json())
         .then((data) => {
           if (data.error) {
-            const formError = document.querySelector('.form__error');
-            formError.textContent = data.message;
-            formError.style.opacity = '1';
-            formError.style.visibility = 'visible';
-            setTimeout(function () {
-              formError.textContent = 'There are no errors';
-              formError.style.opacity = '0';
-              formError.style.visibility = 'hidden';
-            }, 5000);
+            const labelControl = passwordInput.parentElement;
+            const span = labelControl.querySelector('.label__error');
+
+            span.className = 'label__error label__error--visible';
+            span.innerText = data.message;
+            passwordInput.className = 'form__input form__input--error';
+            usernameInput.className = 'form__input';
           } else {
             window.location.href = '#/success';
           }
@@ -52,9 +109,8 @@ route('/', 'login', function () {
         .catch((err) => {
           console.error('There has been a problem with your fetch operation:', err);
         });
-
-    const loginForm = document.querySelector('.login__form');
-    loginForm.reset();
+      loginForm.reset();
+    }
   });
 });
 
